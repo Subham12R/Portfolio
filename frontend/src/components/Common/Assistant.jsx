f048f0113ebadd911a5f6d6096bf062803def73e
Subham Karmakar <rikk4335@gmail.com>
2025-12-12 19:01:12 +0530

    changed pfp

--- frontend/src/components/Common/Assistant.jsx
+++ frontend/src/components/Common/Assistant.jsx
@@ -7,7 +7,7 @@ import { BiPaperPlane } from "react-icons/bi";
 import { RiCloseFill, RiAddFill } from "react-icons/ri";
 import { useTheme } from '../../contexts/ThemeContext';
 import apiService from '../../services/api';
-import profileImage from '../../assets/profile.png';
+import profileImage from '../../assets/pfp.jpg';
 
 const Assistant = () => {
   const [isOpen, setIsOpen] = useState(false);
